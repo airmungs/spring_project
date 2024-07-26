613fc1f4dcfbb3f9b7b531e1567f959f18a65bc8
airmungs <airmungs@gmail.com>
2024-07-26 11:47:25 +0900

    ajaxok

--- shopping_mall/src/main/java/com/shopping/www/web_Controller.java
+++ shopping_mall/src/main/java/com/shopping/www/web_Controller.java
@@ -1,8 +1,14 @@
 package com.shopping.www;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class web_Controller {
-
+	
+	@GetMapping("/ajaxok.do")
+	public String ajaxok() {
+		
+		return null;
+	}
 }
